8c33c7ec8620c35592d00d34c0d8c4d5208ab34f
Yoji Ueda <bluezbz@gmail.com>
2014-09-10 21:51:42 +0900

    Initial Commit

---
+++ APISiOSExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  APISiOSExample
+//
+//  Created by 植田 洋次 on 2014/09/10.
+//  Copyright (c) 2014年 Yoji Ueda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ APISiOSExample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  APISiOSExample
+//
+//  Created by 植田 洋次 on 2014/09/10.
+//  Copyright (c) 2014年 Yoji Ueda. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
